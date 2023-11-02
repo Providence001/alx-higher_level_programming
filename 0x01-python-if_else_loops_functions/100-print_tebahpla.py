264a874fca1b965ccf8896be4005d5a6ffdf41f1
Chiemezie Dike <138778055+Providence001@users.noreply.github.com>
2023-11-02 14:57:41 +0100

    Add files via upload

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(26):
+for i in range(ord('Z'), ord('A')-1, -1):
     if i % 2 == 0:
-        print('{:c}'.format(122 - i), end='')
-    else:
-        print('{:c}'.format(90 - i), end='')
+        i = i + 32
+    print("{:c}".format(i), end='')
